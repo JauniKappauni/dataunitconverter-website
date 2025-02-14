8ebfefd7ad0475a73eecbaef8518a27ee9dd92c3
JauniKappauni <social.jauni@gmail.com>
2025-02-14 21:57:06 +0100

    Added retrieval of inputted value via the request variable Added return of output variable Added name attribute to number input and also required attribute to prevent abuse

--- app.py
+++ app.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def function1():
-    return render_template("index.html")
+    output = ""
+    if request.method == "POST":
+        input = float(request.form.get("input"))
+        output = input / 1024
+    return render_template("index.html",output=output)
 
 if __name__ == "__main__":
     app.run(debug=True)
